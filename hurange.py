aa54e529770b4d2d9c66ed26fcaf22e1c01d8c24
jodathecoda <jodathecoda@yahoo.com>
2021-11-15 19:54:30 +0200

    Ready

--- hurange.py
+++ hurange.py
@@ -7,9 +7,9 @@ cwd = os.getcwd()
 
 root = Tk() 
 
-hu_img                      = ImageTk.PhotoImage(Image.open(cwd + "\\ranges\\headsup.png"))
+hu2x_img                    = ImageTk.PhotoImage(Image.open(cwd + "\\ranges\\slumbot2x.png"))
 pushfold_img                = ImageTk.PhotoImage(Image.open(cwd + "\\ranges\\pushfold.png"))
-notsupported_img            = ImageTk.PhotoImage(Image.open(cwd + "\\ranges\\range_not_supported.png"))
+hu3x_img                    = ImageTk.PhotoImage(Image.open(cwd + "\\ranges\\slumbot3x.png"))
 
 # A Radiobutton to toggle between images
 bigblinds = IntVar()
@@ -19,21 +19,27 @@ def call():
     #heads up
     if bigblinds.get() == 10:
         canvas.create_image((2, 2), image=pushfold_img, anchor=NW)
+    elif bigblinds.get() == 11:
+        canvas.create_image((2, 2), image=hu2x_img, anchor=NW)
+    elif bigblinds.get() == 12:
+        canvas.create_image((2, 2), image=hu3x_img, anchor=NW)
     else:
-        canvas.create_image((2, 2), image=hu_img, anchor=NW)          
+        pass      
 
 #Description
 #L1=Label(root, text ="R=raise  G=call  B=fold").grid(row=0, column=0, columnspan=5, sticky=S)
 #Big blinds selection
-R4=Radiobutton(root, text="10", variable=bigblinds, value=10, command=call)
+R4=Radiobutton(root, text="<10", variable=bigblinds, value=10, command=call)
 R4.grid(row=1, column=0, sticky=N+E)
-R5=Radiobutton(root, text="11", variable=bigblinds, value=11, command=call)
-R5.grid(row=1, column=1, sticky=N+E)
+R5=Radiobutton(root, text="2x", variable=bigblinds, value=11, command=call)
+R5.grid(row=2, column=0, sticky=N+E)
+R6=Radiobutton(root, text="3x", variable=bigblinds, value=12, command=call)
+R6.grid(row=3, column=0, sticky=N+E)
 
 # A canvas for mouse events and image drawing
 canvas = Canvas(root, height=330, width=330,)
 canvas.grid(column=5, row=0, rowspan=6, sticky=W)
-canvas.create_image((2, 2), image=hu_img, anchor=NW)
+canvas.create_image((2, 2), image=hu3x_img, anchor=NW)
 
 # Enter event loop
 root.mainloop()
